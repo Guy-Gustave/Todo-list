1171043bcfe6f32965da049008cf242c2f101a9f
Visvaldas <visva.rapalis@gmail.com>
2020-11-24 16:14:33 +0200

    Update structure of new item

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Dom Selectors
 const input = document.querySelector('.todo-input');
+// const inputs = document.querySelectorAll('input')
+// const todo = document.querySelector('#todos .todo');
+// todo.classList.toggle('done');
 const notCompleted = document.querySelector('.notCompleted');
 const completed = document.querySelectorAll('.completed');
 const addButton = document.querySelector('.addButton');
@@ -13,15 +16,23 @@ addButton.addEventListener('click', addItem);
 function addItem(e) {
   e.preventDefault();
   console.log('You clicked me!!!!');
+  // inputs[2].checked
   const item = document.createElement('div');
   item.classList.add('item');
   const check = document.createElement('INPUT');
   check.setAttribute('type', 'checkbox');
+  const edit = document.createElement('button');
+  edit.classList.add('editButton');
+  edit.textContent = 'Edit';
+  const del = document.createElement('button');
+  del.textContent = 'Delete';
+  del.classList.add('removeButton');
 
   const paragraph = document.createElement('p');
   paragraph.innerText = input.value;
-  paragraph.appendChild(check);
-
+  item.appendChild(check);
   item.appendChild(paragraph);
+  item.appendChild(edit);
+  item.appendChild(del);
   notCompleted.appendChild(item);
 }
