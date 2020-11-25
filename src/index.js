52525bdcaed885d1e4f9e07c3fcbe1dce2eea54c
Visvaldas <visva.rapalis@gmail.com>
2020-11-25 10:27:49 +0200

    Add delete functioanality and local storage

--- src/index.js
+++ src/index.js
@@ -1,48 +1,88 @@
-// Dom Selectors
-const input = document.querySelector('.todo-input');
-// const inputs = document.querySelectorAll('input')
-// const todo = document.querySelector('#todos .todo');
-// todo.classList.toggle('done');
-const notCompleted = document.querySelector('.notCompleted');
-const completed = document.querySelectorAll('.completed');
-const addButton = document.querySelector('.addButton');
-const editButton = document.querySelector('.editButton');
-const removeButton = document.querySelector('.removeButton');
-// Event listeners
-addButton.addEventListener('click', addItem);
-// editButton.addEventListener('click', editItem);
-removeButton.addEventListener('click', removeItem);
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItemsList = document.querySelector('.todo-items');
 
-function clearInputs() {
-  document.querySelector('.todo-input').value = '';
+let todos = [];
+
+todoForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  addTodo(todoInput.value);
+});
+
+function addTodo(item) {
+  if (item !== '') {
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false,
+    };
+
+    todos.push(todo);
+    addToLocalStorage(todos);
+    todoInput.value = '';
+  }
 }
 
-function addItem(e) {
-  e.preventDefault();
-  // inputs[2].checked
-  const item = document.createElement('div');
-  document.body.appendChild(item);
-  item.classList.add('item');
-  const check = document.createElement('INPUT');
-  check.setAttribute('type', 'checkbox');
-  const edit = document.createElement('button');
-  edit.classList.add('editButton');
-  edit.textContent = 'Edit';
-  const del = document.createElement('button');
-  del.textContent = 'Remove';
-  del.classList.add('removeButton');
+function renderTodos(todos) {
+  todoItemsList.innerHTML = '';
 
-  const paragraph = document.createElement('p');
-  paragraph.innerText = input.value;
-  item.appendChild(check);
-  item.appendChild(paragraph);
-  item.appendChild(edit);
-  item.appendChild(del);
-  notCompleted.appendChild(item);
-  clearInputs();
+  todos.forEach(function (item) {
+    const checked = item.completed ? 'checked' : null;
+    const li = document.createElement('li');
+    li.setAttribute('class', 'item');
+    li.setAttribute('data-key', item.id);
+    if (item.completed === true) {
+      li.classList.add('checked');
+    }
+
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${checked}>
+      ${item.name}
+      <button class="delete-button">X</button>
+    `;
+    todoItemsList.append(li);
+  });
 }
 
-function removeItem() {
-  const item = document.querySelector('.item');
-  item.remove();
+function addToLocalStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  renderTodos(todos);
 }
+
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+  if (reference) {
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
+}
+
+function toggle(id) {
+  todos.forEach(function (item) {
+    if (item.id == id) {
+      item.completed = !item.completed;
+    }
+  });
+
+  addToLocalStorage(todos);
+}
+
+function deleteTodo(id) {
+  todos = todos.filter(function (item) {
+    return item.id != id;
+  });
+
+  addToLocalStorage(todos);
+}
+
+getFromLocalStorage();
+
+todoItemsList.addEventListener('click', function (event) {
+  if (event.target.type === 'checkbox') {
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+
+  if (event.target.classList.contains('delete-button')) {
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
